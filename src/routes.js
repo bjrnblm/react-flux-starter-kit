b0ca7ae58d31b797a6f69fb8000b92c901974f6d
Björn Blom <bjornblom@gmail.com>
2015-09-15 17:44:36 +0200

    used Router for links and added not found page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@
 var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
 var Route = Router.Route;
 
 var routes = (
@@ -10,6 +11,7 @@ var routes = (
         <DefaultRoute handler={require('./components/homePage')} />
         <Route name="authors" handler={require('./components/authors/authorPage')} />
         <Route name="about" handler={require('./components/about/aboutPage')} />
+        <NotFoundRoute handler={require('./components/notFoundPage')} />
     </Route>
 );
 
