b250bd8416c6db86e366ae675f5dc7945507af8b
Björn Blom <bjornblom@gmail.com>
2015-09-18 17:32:59 +0200

    added toasts and small tweaks to author page

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -4,6 +4,14 @@ var React = require('react');
 var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
+
+    propTypes: {
+        author: React.PropTypes.object.isRequired,
+        onSave: React.PropTypes.func.isRequired,
+        onChange: React.PropTypes.func.isRequired,
+        errors: React.PropTypes.object
+    },
+
     render: function() {
         return (
             <form>
@@ -13,12 +21,14 @@ var AuthorForm = React.createClass({
                     label="First Name"
                     value={this.props.author.firstName}
                     onChange={this.props.onChange}
+                    error={this.props.errors.firstName}
                 />
                 <Input
                     name="lastName"
                     label="Last Name"
                     value={this.props.author.lastName}
                     onChange={this.props.onChange}
+                    error={this.props.errors.lastName}
                 />
                 <input
                     type="submit"
